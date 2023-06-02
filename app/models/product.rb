12aad2113ae71068c28d1d5ec2d6e86fa03038a3
Camila Meireles <camiladmeireles@hotmail.com>
2023-06-02 15:01:44 -0300

    ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
   belongs_to :user
   validates :name, :description, :stock, :price, :photos, presence: true
   has_many_attached :photos
-  CATEGORY = ["Cestarias", "Enfeites", "Cozinha", "Cerâmica", "Decoração", "Vasos", "Luminárias", "Bordados"]
+  CATEGORY = ["Cestarias", "Enfeites", "Cozinha", "Cerâmica", "Decoração", "Vasos", "Luminárias", "Bordados", "Descontos"]
 end
