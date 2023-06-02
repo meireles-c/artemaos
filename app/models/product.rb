bd314be5a4d895d7fcf0dce8712b1de7b2f57a37
Natalia Ozawa <nati.ozawa@gmail.com>
2023-06-02 15:53:12 -0300

    done

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   has_many :orders
   belongs_to :user
   validates :name, :description, :stock, :price, :photos, presence: true
+  # validates_presence_of :photos, on: :new
   has_many_attached :photos
   CATEGORY = ["Cestarias", "Enfeites", "Cozinha", "Cerâmica", "Decoração", "Vasos", "Luminárias", "Bordados"]
 end
